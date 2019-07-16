4cc2e56aa80ec61782cd61cb9302a044286f9ef1
MARQyie <50098649+MARQyie@users.noreply.github.com>
2019-07-16 16:40:27 +0200

    Add files via upload

---
+++ Balance_paneldata.py
@@ -0,0 +1,9 @@
+# Balance panel
+''' This function takes a pandas dataframe that is structured as a unbalanced
+    panel and balances it. 
+    
+    Clean the data beforehand!
+    Do not use a df with multiindex
+    
+    Input: pandas dataframe, name entity column, name time column.
+    '''
