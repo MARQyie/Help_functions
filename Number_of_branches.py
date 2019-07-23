051b6f619f1d4219ceee7327fb1808937235a387
MARQyie <50098649+MARQyie@users.noreply.github.com>
2019-07-23 17:03:28 +0200

    Version 3

---
+++ Number_of_branches.py
@@ -0,0 +1,6 @@
+# Number of Branches (Summary of deposits)
+''' This function reads in the summary of deposits data and returns a pandas
+    dataframe with IDRSSD, date and number of branches
+
+    Packages needed: Pandas
+    '''
